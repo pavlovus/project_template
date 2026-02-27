321872603b2e021a00801582dbf93d34476de2b8
pavlovus <pavlovus7002@gmail.com>
2026-02-28 00:50:18 +0200

    Add empty io functions

---
+++ app/io/input.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+def input_from_console(message: str) -> str:
+    pass
+
+def input_from_file(path: str) -> str:
+    pass
+
+def input_from_file_with_pandas (path: str) -> str:
+    pass
---
+++ app/io/output.py
@@ -0,0 +1,5 @@
+def output_to_console(message: str) -> None:
+    pass
+
+def output_to_file(message: str, path: str) -> None:
+    pass
